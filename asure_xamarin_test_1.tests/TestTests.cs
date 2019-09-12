3579481b6326a7c94464c4b7f44d14c7634d702c
Deo Kalule <deo@pnautomation.com>
2019-09-12 13:18:28 -0400

    init

---
+++ asure_xamarin_test_1.tests/TestTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace asure_xamarin_test_1.tests
+{
+    [TestClass]
+    public class TestTests
+    {
+        [TestMethod]
+        public void TestFail()
+        {
+            Assert.IsTrue(false);
+        }
+
+
+        [TestMethod]
+        public void TestPass()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
